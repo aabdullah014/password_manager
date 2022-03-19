9196f57173955c60b894c7c68b0533eddcb7f9f7
Abdulrahman Abdullah <96795264+aabdullah014@users.noreply.github.com>
2022-03-18 19:53:18 -0700

    Implemented Jinja Templating
    Added more routes, as well as Jinja templating to interact with Python expressions and variables.

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "login route"
+    return render_template("login.html", arg = "Test")
 
 @auth.route('/logout')
 def logout():
@@ -12,4 +12,4 @@ def logout():
 
 @auth.route('/register')
 def register():
-    return "register route"
+    return render_template("register.html")
