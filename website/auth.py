f86d9b129272cca0c68fc4beb9bbfbd378e9c918
Abdulrahman Abdullah <96795264+aabdullah014@users.noreply.github.com>
2022-03-20 16:52:38 -0700

    Fixed password hashing
    Fixed error that arose when hashing with werkzeug; password exceeded character limit and was not reproducible without knowing the salt. Incorporated a different hashing method that doesn't use salting as then it can be used to login.

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,9 @@
+from tabnanny import check
 from flask import Blueprint, render_template, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+import hashlib
 import requests
+
 
 auth = Blueprint('auth', __name__)
 
@@ -7,13 +11,17 @@ auth = Blueprint('auth', __name__)
 def login():
     if request.method == "POST":
         username = request.form.get('username')
-        password = request.form.get('password')
-
-        payload = {'username': username, 'password': password}
+        password = request.form.get('password').encode()
+        hash_password = hashlib.sha256(password).hexdigest()
+        payload = {'username': username, 'password': hash_password}
+        print(password)
+        print(payload)
         url = "https://family-task-api.herokuapp.com/auth"
 
         response = requests.post(url, json = payload)
-        access_token = response.json()['access_token']
+        print(response.json())
+        # access_token = response.json()['access_token']
+        # print(access_token)
 
     return render_template("login.html")
 
@@ -25,12 +33,14 @@ def logout():
 def register():
     if request.method == "POST":
         username = request.form.get('username')
-        password = request.form.get('password')
+        password = request.form.get('password').encode()
 
-        payload = {'username': username, 'password': password}
+        #only 20 characters because server won't take more than 20.
+        hash_password = hashlib.sha256(password).hexdigest()
+
+        payload = {'username': username, 'password': hash_password}
         url = "https://family-task-api.herokuapp.com/register"
 
-        response = requests.post(url, json = payload)
 
         if len(username) < 4:
             flash('Username must be at least 4 characters!', category = "error")
@@ -38,6 +48,10 @@ def register():
             flash('Password must be at least 6 characters!', category = "error")
         else:
             #add user to db
+            response = requests.post(url, params = payload)
+            print(password)
+            print(payload)
+            print(response.json())
             flash('Successfully registered!', category = "success")
 
     return render_template("register.html")
