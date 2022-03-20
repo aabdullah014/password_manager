990629bcf83ac27e21bae7f8f048fad5e3078dec
Abdulrahman Abdullah <96795264+aabdullah014@users.noreply.github.com>
2022-03-19 17:40:19 -0700

    connected login and register routes with taskAPI
    Initially used urllib to connect with API, then switched to requests library as it was easier.

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,43 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import requests
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template("login.html", boolean = True)
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        payload = {'username': username, 'password': password}
+        url = "https://family-task-api.herokuapp.com/auth"
+
+        response = requests.post(url, json = payload)
+        access_token = response.json()['access_token']
+
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "logout route"
 
-@auth.route('/register')
+@auth.route('/register', methods = ['GET', 'POST'])
 def register():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        payload = {'username': username, 'password': password}
+        url = "https://family-task-api.herokuapp.com/register"
+
+        response = requests.post(url, json = payload)
+
+        if len(username) < 4:
+            pass
+        elif len(password) < 6:
+            pass
+        else:
+            #add user to db
+            pass
+
     return render_template("register.html")
